8b96ce53e3c01c8cee0eecbb5b95f3ed466be7cf
Gabrilinsky <gabrielnaranjo@liceovvh.cl>
2026-09-25 21:13:38 -0300

    avance

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,47 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const contenedor = document.getElementById("videoContainer");
-  const video = document.getElementById("miVideo");
+document.addEventListener("DOMContentLoaded", function () {
+    const formEmail = document.getElementById("loginForm");
+    const inputEmail = document.getElementById("emailInput");
 
-  if (contenedor && video) {
-    contenedor.addEventListener("mouseenter", function() {
-      contenedor.classList.add("activo");
-      video.pause();
-    });
+    if (formEmail) {
+        formEmail.addEventListener("submit", function (event) {
+            event.preventDefault(); // Evita que la página se recargue
 
-    contenedor.addEventListener("mouseleave", function() {
-      contenedor.classList.remove("activo");
-      video.play();
-    });
-  }
+            const correo = inputEmail.value.trim();
+
+            if (correo !== "") {
+                alert("¡Bienvenido/a! Has ingresado con el correo: " + correo);
+
+                
+                inputEmail.value = "";
+            }
+        });
+    }
+    const botonesMas = document.querySelectorAll(".btn-mas");
+    const spanNum = document.querySelector(".num");
+
+    if (spanNum && botonesMas.length > 0) {
+        let contador = 0;
+
+        botonesMas.forEach(function (boton) {
+            boton.addEventListener("click", function (event) {
+                event.preventDefault();
+                contador++;
+                spanNum.textContent = contador;
+            });
+        });
+    }
+    const contenedor = document.getElementById("videoContainer");
+    const video = document.getElementById("miVideo");
+
+    if (contenedor && video) {
+        contenedor.addEventListener("mouseenter", function () {
+            contenedor.classList.add("active");
+            video.pause();
+        });
+
+        contenedor.addEventListener("mouseleave", function () {
+            contenedor.classList.remove("active");
+            video.play();
+        });
+    }
 });
